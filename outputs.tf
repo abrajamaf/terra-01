533b5db5c29059ae876464c77d49d9823c4ac67f
Abraham Alvarado Fuentes <abrajam.alvarado@gmail.com>
2023-05-12 14:53:16 -0600

    correct-01 on outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,12 @@
-output "server_name" {
+output "nombre_del_server" {
   description = "Nombre de la instancia"
-  value = huaweicloud_compute_instance.terra001
+  value = huaweicloud_compute_instance.terra001.name
 }
 output "internal_ip" {
   description = "IP interna"
-  value = huaweicloud_compute_instance.fixed_ip_v4
+  value = huaweicloud_compute_instance.terra001.access_ip_v4
+}
+output "public_ip" {
+  description = "IP publica"
+  value = huaweicloud_compute_instance.terra001.public_ip
 }
